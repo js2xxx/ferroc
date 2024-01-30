777f44571b57f90d0bf089e1fa060b21997b130d
徐启航 <akucxy@163.com>
2024-01-30 16:40:21 +0800

    Add C binding generation

---
+++ build.rs
@@ -0,0 +1,15 @@
+fn main() {
+    #[cfg(feature = "c")]
+    generate_c_bindings();
+}
+
+#[cfg(feature = "c")]
+fn generate_c_bindings() {
+    let crate_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+    cbindgen::Builder::new()
+        .with_crate(&crate_dir)
+        .with_config(cbindgen::Config::from_root_or_default(crate_dir))
+        .generate()
+        .expect("failed to generate C bindings")
+        .write_to_file("ferroc.h");
+}
